50ae0fdabb295171c335bb60218b2e5c6de9dc23
David Sloan <david.sloan@d3r.com>
2018-11-27 21:37:25 +0000

    Add: ident for connections

--- quiz.js
+++ quiz.js
@@ -8,24 +8,115 @@
 var app  = require('express')(),
     http = require('http').Server(app),
     io   = require('socket.io')(http),
-    port = process.env.PORT || 3000;
+    uuid = require('uuid');
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/views/join.html');
-});
+var port = process.env.PORT || 3000;
 
-io.on('connection', function(socket){
-  console.log('a user connected');
+var sContestant = io.of('/contestant'),
+    sQuizMaster = io.of('/quizmaster'),
+    sDashboard  = io.of('/dashboard');
 
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+var connections = {
+    contestants: [],
+    quizmasters: [],
+    dashboards:  []
+};
 
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-});
-
-http.listen(port, function(){
+// ----------------------------------------------------------------------------
+// Client views
+// ----------------------------------------------------------------------------
+http.listen(port, function () {
     console.log('listening on *:' + port);
 });
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/views/contestant.html');
+})
+.get('/quizmaster', function (req, res) {
+    res.sendFile(__dirname + '/views/quizmaster.html');
+});
+
+// ----------------------------------------------------------------------------
+// Handle connections
+// ----------------------------------------------------------------------------
+function ident (type, cid, socket) {
+    var newConnection = false;
+    if (cid == null) {
+        cid = uuid.v4();
+        socket.emit('id', cid);
+        newConnection = true;
+    } else {
+        console.log('[IDENT] Existing cid: ' + cid);
+    }
+
+    switch (type) {
+        case 'contestant':
+            if (newConnection || connections.contestants[cid] == undefined)
+                connections.contestants[cid] = new oContestant(cid, socket);
+            else
+                connections.contestants[cid].setSocket(socket);
+            break;
+        case 'quizmaster':
+            if (newConnection || connections.quizmaster[cid] == undefined)
+                connections.quizmasters[cid] = new oContestant(cid, socket);
+            else
+                connections.quizmasters[cid].setSocket(socket);
+            break;
+    }
+    return cid;
+}
+
+sQuizMaster.on('connection', function (socket) {
+    var cid = null;
+    console.log('[SOCKET] QM connected on socket: ' + socket.id);
+
+    socket.on('ident', function(id) {
+        cid = ident('quizmaster', id, socket);
+    });
+
+    // io.emit('id', cid);
+
+    socket.on('disconnect', function () {
+        console.log('QM disconnected');
+        // connections.quizmasters[cid] = null;
+    });
+});
+
+sContestant.on('connection', function (socket) {
+    var cid = null;
+    console.log('[SOCKET] Contestant connected on socket: ' + socket.id);
+
+
+    socket.on('ident', function (id){
+        cid = ident('contestant', id, socket);
+    });
+
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', function () {
+        // console.log('contestant [' + cid + '] disconnected');
+        // connections.contestants[cid] = null;
+    });
+});
+
+// ----------------------------------------------------------------------------
+// Objects
+// ----------------------------------------------------------------------------
+function oContestant (cid, socket) {
+  this.id = cid;
+  this.socket = socket;
+  console.log('New Contestant[' + cid + ']');
+}
+oContestant.prototype.setSocket = function (socket) {
+  this.socket = socket;
+}
+
+function oQuizMaster (cid, socket) {
+  this.id = cid;
+  this.socket = socket;
+  console.log('New QuizMaster[' + cid + ']');
+}
+oQuizMaster.prototype.setSocket = function (socket) {
+  this.socket = socket;
+}
