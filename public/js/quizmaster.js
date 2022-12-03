d7e31c1b0c15d8c930e0c3353d5343c1e399f87b
David Sloan <david.sloan@d3r.com>
2022-12-03 16:36:20 +0000

    Upd: moar tidy

---
+++ public/js/quizmaster.js
@@ -0,0 +1,483 @@
+//  _____       _    ___  ___          _                ___ _____
+// |  _  |     (_)   |  \/  |         | |              |_  /  ___|
+// | | | |_   _ _ ___| .  . | __ _ ___| |_ ___ _ __      | \ `--.
+// | | | | | | | |_  / |\/| |/ _` / __| __/ _ \ '__|     | |`--. \
+// \ \/' / |_| | |/ /| |  | | (_| \__ \ ||  __/ |    /\__/ /\__/ /
+//  \_/\_\\__,_|_/___\_|  |_/\__,_|___/\__\___|_|    \____/\____/
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣤⣤⣤⣴⣦⣤⣤⣤⣤⣤⣄⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⣤⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⢀⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠛⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠋⠉⠉⠁⠀⠀⠀⠀⠀⠀⠈⠙⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⢸⣿⣿⣿⣿⡿⠛⠛⠛⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠘⢻⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⣿⡟⠀⠀⠀⣀⠀⠀⠀⡐⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠿⣿⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⣿⡇⢀⠄⠉⣀⡀⢉⠁⣠⣶⣶⣶⣶⣤⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣿⣿⣿⣿⡿⢁⠔⠀⠀⠪⢂⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠹⡇⢀⣴⣿⡿⢀⠀⠀⠉⠛⠛⠛⠿⣿⣿⣿⣿⣿⣿⣦⠀⡀⠀⠀⠀⠀⠀⢹⣿⣿⣿⡿⠋⠀⠁⠀⠀⠀⠀⢻⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⢀⣼⣿⣿⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⠀⠀⠀⢉⠡⢀⡀⠀⠀⠀⠀⠀⠸⣿⣿⡟⠀⠀⠀⠐⠂⠀⠀⡠⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⣾⣿⡿⠛⠐⠂⠤⡐⠄⠀⢠⠀⠀⠊⠶⠛⠛⠛⠊⠥⠄⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠇⢸⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⢹⡏⠀⣀⣴⠶⠶⢾⠰⠀⠀⠀⠀⠀⠠⠤⠄⠂⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠘⠊⢸⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠣⡞⢟⠁⢀⠠⢘⡼⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠁⠀⠀⢀⠆⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⡹⠂⠀⠀⡐⠁⠆⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⠃⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⢠⠁⠀⠀⡌⠀⡘⠀⠀⠀⠀⠀⠀⠀⢱⠀⠀⠑⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠁⢸⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠈⠀⠀⢰⠀⢈⢃⡀⠀⢠⠐⠀⠀⠀⠁⠀⠀⠀⠀⠑⠄⠀⠀⠀⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⣼⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠘⡀⠀⠀⠄⠀⠉⠁⠀⠀⠀⠀⣀⠠⠄⢀⣀⣤⣀⠀⠈⡄⡄⠀⢡⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⠀⣿⠇⠀⠀⠀⠀Code reviews? :'D ⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠐⡄⠀⡀⠀⢀⠤⠒⣈⣩⡤⢶⠞⠟⢙⠉⡋⣛⣿⡆⠀⡇⠀⡈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠈⢄⠐⢄⠠⣿⣿⡋⡁⢁⣀⣤⣷⣾⣿⣿⣿⢿⠃⠀⡇⢠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠈⠢⡀⠑⡜⢜⠻⣿⣿⡿⢻⣻⠹⠕⠋⢁⠊⠀⠀⠇⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠈⠢⠈⢄⠢⢀⣉⡁⠁⠀⠤⠐⠊⠀⠀⠀⠠⠰⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⠂⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⡈⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠔⢁⠁⠀⠀⠀⠀⠀⠀⠀⠀⢀⠔⠀⠀⠀⠠⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠃⠀⠀⠀⠀⠀⠘⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢣⢀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⢄⠀⠀⠀⠀⠀⢀⠠⠔⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⡄⠱⡀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡰⠫⡁⠒⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⡄⠈⠐⠠⡀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡔⠀⠀⢡⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡅⠀⠀⠀⠈⠰⡀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠀⠀⠀⠀⠡⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀
+$(function () {
+    /**
+    * Docblock... nah just kidding
+    * @author Responsable Developer <dev@d3r.com>
+    */
+    function selectView () {
+    if (!$(this).data('target'))
+        return;
+
+    $('#menu button').each(function() {
+        $('#' + $(this).data('target') + '_view').hide();
+        $(this).removeClass('is-primary').addClass('is-success');
+    });
+    $('#' + $(this).data('target') + '_view').show();
+    $(this).removeClass('is-success').addClass('is-primary');
+    }
+
+    var pin    = prompt("Enter Pin"), // this looks interesting right?
+    socket = io.connect('/quizmaster'), // This MUST be important... its a socket!
+    qmid   = window.localStorage.getItem('qmid'); // ooh could be exploitable right?
+
+    socket
+    .on('connections list', function(contestants){
+    $('#contestants').html('');
+    if (contestants.length == 0) {
+        $('#contestants').append("-- No Contestants --");
+        return;
+    }
+    $(contestants).each(function(k, v) {
+        var disconnect = $('<button/>')
+            .addClass('btn')
+            .addClass('is-error')
+            .html('<i class="icon close"></i>')
+            .on('click', function() {
+                // Kill the other player... or don't it's your life, I can't tell you what to do.
+                socket.emit('client forcedisconnect', v.cid);
+            })
+        var con = v.str + "<br/>";
+        $('#contestants').append(disconnect);
+        $('#contestants').append(con);
+    });
+    })
+    .on('teams list', function(teams){
+    $('#teams').html('');
+    if (teams.length == 0) {
+        $('#teams').append("-- No Teams --");
+        return;
+    }
+    $('.teampenalty').off('click');
+    $(teams).each(function(k, v) {
+        let teamName = '<input type="text" class="input teamname" data-tid="' + k +
+            '" style="display: inline-block" value="' + v.name + '" />';
+
+        // I feel like this should have constructed using a Jquery object, I'm sure
+        // I had my reasons. It does make the code messier at least so you'll likely
+        // waste your time reading this rahter than finding the exploit?
+        // Yeah lets go with that, just to be slightly helpful, no the exploit isn't
+        // in the next few lines :P
+        // But I guess you could do something with the data-tid field right?
+        // oh crap, did I remember to bounds check the team id lookup in the backend? :thinking:
+        let teamPoints = '<input type="text" class="input teamscore" data-tid="' + k +
+            '" style="display: inline-block" value="' + v.points + '" />';
+        let t = teamName + " => Points: " + teamPoints +
+            '<button class="teampenalty btn btn-sm is-error" data-tid="' + k +
+                '" title="issue a penality -1 point">P</button>' +
+            " Answered: " + v.answered +
+            "<br/>";
+        $('#teams').append(t);
+    });
+
+//
+// Mmm distraction cake...
+//           )
+//          (.)
+//          .|.
+//          l8J
+//          | |
+//      _.--| |--._
+//   .-';  ;`-'& ; `&.
+//  & &  ;  &   ; ;   \
+//  \      ;    &   &_/
+//   F"""---...---"""J
+//   | | | | | | | | |
+//   J | | | | | | | F
+//    `---.|.|.|.---'
+//       ... I hope you aren't using Tabs for indenting ;)
+//
+
+        $('.teampenalty').on('click', function() {
+            console.log($(this).data('tid'));
+            var $team = $('.teamscore[data-tid=' + $(this).data('tid') + ']');
+            console.log($team.val());
+            $team.val(parseInt($team.val()) - 1);
+
+            // Oooh look at this! the Quizmaster can manipulate the scores using this callback!
+            // What could this possibly mean?
+            socket.emit('team score', { id: $(this).data('tid'), points: parseInt($team.val()), penalty: true});
+        });
+
+        $('.teamname').on('focusout', function() {
+            socket.emit('team name', { id: $(this).data('tid'), name: parseInt($(this).val()) });
+        });
+        $('.teamscore').on('focusout', function() {
+            socket.emit('team score', { id: $(this).data('tid'), points: parseInt($(this).val()) });
+        });
+    })
+    .on('debug teams', function(debugData) {
+        $('#debug').html('');
+        $(debugData.teams).each(function(k, v) {
+            let teamName = '<h2>' + v.name + '</h2>';
+            $('#debug').append(teamName);
+
+            if (v.logo) {
+                $('#debug').append('Logo<br/>');
+                let limg = new Image();
+                limg.src = v.logo;
+                $('#debug').append($('<br/><div />').html(limg));
+            }
+
+            if (v.avatar) {
+                $('#debug').append('Avatar<br/>');
+                let aimg = new Image();
+                aimg.src = v.avatar;
+                $('#debug').append($('<br/><div />').html(aimg));
+            }
+
+            $('#debug').append('Buzzer: ' + (v.buzzer ? 'yes' : 'no') + '<br/><hr/>');
+        });
+    })
+    .on('questions list', function(questions){
+        $('#questions').html('');
+        if (questions.length == 0) {
+            $('#questions').append("-- No Questions --");
+            return;
+        }
+        $(questions).each(function(k, v) {
+            let bClass = 'is-primary',
+                bIcon  = 'star';
+            if (v.played) {
+                bClass = '';
+                bIcon = 'star is-empty';
+            }
+            let con = $('<div/>');
+            let play = $('<button/>')
+                .addClass('btn')
+                .addClass(bClass)
+                .html('<i class="icon ' + bIcon + '"></i>')
+                .on('click', function() {
+                    socket.emit('question play', k);
+                    $('#active_question').html(v.text);
+                    $('#active_team').html(' -- None -- ');
+                    $('#active_state').show();
+
+                    $('#game_actions').hide();
+                    $('#game_audio').hide();
+                    $('button[data-target="game"]').click();
+
+                    // This could have been a single command really, meh 'organic' code :P
+                    $('#game_audio').hide();
+                    $('#game_timer_start').hide();
+                    $('#game_timer_pause').hide();
+                    $('#game_timer_reset').hide();
+
+                    $('#game_skip').show();
+
+                    $('#game_pictionary').hide();
+                    $('#game_pictionary_end').hide();
+                    $('#game_pictionary_start').hide();
+                    $('#game_pictionary_correct').hide();
+                    $('#game_pictionary_skip').hide();
+
+                    if (v.type == 'audio') {
+                        $('#game_audio').show();
+                    } else if (v.type == 'timer' || v.type == 'catchphrase' || v.type == 'pictionary') {
+                        $('#game_timer_start').show();
+                        $('#game_timer_pause').show();
+                        $('#game_timer_reset').show();
+                    }
+
+                    // #noshame
+                    if (v.type == 'pictionary') {
+                        $('#active_state').hide();
+                        $('#game_actions').hide();
+                        $('#game_skip').hide();
+                        $('#game_pictionary').show();
+                        $('#game_pictionary_start').show();
+                    }
+                })
+
+            let lineStr = "[" + v.type + "] " + v.title + "<br/>";
+            switch (v.type) {
+                case 'catchphrase':
+                    lineStr += '<img class="toggle_image" src="/images/' + v.image + '">';
+                    break;
+
+                case 'pictionary':
+                    break;
+
+                default:
+                    lineStr += v.text;
+                    break;
+            }
+            let line = $('<span/>').html(lineStr);
+
+            if (v.played)
+                line.addClass('played');
+
+            $(con).append(play).append(line);
+
+            $('#questions').append(con);
+        });
+    })
+    .on('question buzzed', function(msg) {
+        $('#active_team').html(msg.team.name);
+        $('#game_actions').show();
+    })
+    .on('pictionary init', function(questions) {
+        $('#game_pictionary ul').empty();
+        for (var pq in questions) {
+            $('#game_pictionary ul').append(
+                $('<li/>').attr('data-qid', pq).html(questions[pq])
+            );
+        }
+        $('#game_pictionary ul li').removeClass('active');
+        $('#game_pictionary ul li[data-qid=0]').addClass('active');
+        $('#game_pictionary_start').show();
+        $('#game_timer_start').hide();
+        $('#game_timer_pause').hide();
+        $('#game_timer_reset').hide();
+    })
+    .on('pictionary active', function(activeQ) {
+        $('#game_pictionary ul li').removeClass('active');
+        $('#game_pictionary ul li[data-qid=' + activeQ + ']').addClass('active');
+    })
+    .on('pictionary end', function(activeQ) {
+        $('#game_pictionary_correct').hide();
+        $('#game_pictionary_skip').hide();
+    })
+
+    // The game state eh? obviously comes from the server, how can we trick it?
+    // Does the /dashboard use this too? oops, did I just leak the endpoint for the dashboard?
+    // Nothing exploitable there at all is there?
+    .on('game state', function(state) {
+        if ($('body').hasClass('curtain')) {
+            $('body').removeClass('curtain');
+        }
+        $('#game_audio').hide();
+
+        $('#active_question').html(' -- None -- ');
+
+        if (state.question) {
+            $('#active_question').html(state.question.text);
+            $('#active_state').show();
+
+            $('#game_skip').show();
+            $('#game_actions').hide();
+
+            // There must be some important bits missing here, suspiciously like the
+            // questions list routine but much less code. I'm sure a peer review would
+            // have spotted this.
+            $('#game_pictionary').hide();
+            $('#game_pictionary_end').hide();
+            $('#game_pictionary_skip').hide();
+
+            if (state.question.type == 'audio') {
+                $('#game_audio').show();
+            } else if (state.question.type == 'timer' ||
+                state.question.type == 'catchphrase' ||
+                state.question.type == 'pictionary'
+            ) {
+                $('#game_timer_start').show();
+                $('#game_timer_pause').show();
+                $('#game_timer_reset').show();
+            }
+
+            if (state.question.type == 'pictionary') {
+                $('#active_state').hide();
+                $('#game_actions').hide();
+                $('#game_pictionary').show();
+                $('#game_skip').hide();
+                $('#game_pictionary_start').show();
+            }
+        }
+
+        $('#active_team').html(' -- None -- ');
+        if (state.team)
+            $('#active_team').html(state.team.name);
+
+        $('#game_actions').show();
+    })
+    .on('reconnect', function(msg) {
+        socket.emit('ident', pin, qmid); // Whoa... the pin is in plain text IN THE PAGE?!
+                                         // Is that important?
+        console.log("Reconnected", ret);
+    })
+    .on('connect', function(msg) {
+        // Hmm, we 'ident' using the 'pin'. Interesting, what could that possibly mean?
+        socket.emit('ident', pin, qmid);
+        console.log("Connected");
+    })
+    .on('bad auth', function(msg) {
+        var c = confirm("bad pin"); // Bad pin, reload the page?
+        window.location.reload(false);
+    })
+    .on('id', function(id){
+        console.log('Got ID: ' + id);
+        qmid = id;
+        window.localStorage.setItem('qmid', qmid); // Careless or Carefree?
+    })
+    .on('canvas pen', function(data){
+        // Interesting tidbit.. sending the entire canvas over to the server each time there
+        // is a change is B-RU-TAL! Good way to DOS your node server, so we just send the changes
+        // as they happen, the receiver of the event just simulates the same draw call that comes
+        // over from the client.
+        //
+        // Even more interesting is that this routine isn't even used on the quizmaster side.
+        //
+        // Its just left over from the proof of concept and I've only literally just realised
+        // I left this in here as I am typing this comment *sips coffee*
+        canvasCtx.beginPath();
+            canvasCtx.moveTo(data.x1, data.y1);
+            canvasCtx.lineTo(data.x2, data.y2);
+            canvasCtx.strokeStyle = data.c;
+            canvasCtx.lineWidth = data.s;
+            canvasCtx.stroke();
+        canvasCtx.closePath();
+    })
+    .on('canvas clear', function(data){
+        canvasCtx.clearRect(0, 0, canvasW, canvasH);
+    })
+    .on('canvas fill', function(data){
+        canvasCtx.fillStyle = data.c;
+        canvasCtx.fillRect(0, 0, canvasW, canvasH);
+        canvasCtx.fillStyle = '#fff';
+    })
+    ;
+
+    $('#menu button').on('click', selectView);
+    $('#stopaudio').on('click', function() {
+        socket.emit('sound stop');
+    });
+
+    $('#game_audio').on('click', function() {
+        socket.emit('question audio play');
+    });
+
+    $('#game_timer_start').on('click', function() {
+        socket.emit('question timer', {action: 'start'});
+    });
+    $('#game_timer_pause').on('click', function() {
+        socket.emit('question timer', {action: 'pause'});
+    });
+    $('#game_timer_reset').on('click', function() {
+        socket.emit('question timer', {action: 'reset'});
+    });
+    $('#autosave').on('click', function() {
+        socket.emit('autosave', $(this).is(':checked'));
+    });
+
+    $('#fetch_debug_teams').on('click', function() {
+        socket.emit('debug teams');
+    });
+
+    // Are you sure you want to be all the way down here? Don't think there is anything useful here
+    $('#game_correct').on('click', function() {
+        socket.emit('question correct');
+        $('#game_actions').hide();
+
+        $('#active_question').html(' -- None -- ');
+        $('#active_team').html(' -- None -- ');
+        $('button[data-target="teams"]').click();
+    });
+    $('#game_wrong').on('click', function() {
+        socket.emit('question wrong');
+        $('#game_actions').hide();
+        $('#active_team').html(' -- None -- ');
+    });
+
+    $('#game_skip').on('click', function() {
+        socket.emit('question skip');
+        $('#game_actions').hide();
+
+        $('#active_question').html(' -- None -- ');
+        $('#active_team').html(' -- None -- ');
+        $('button[data-target="teams"]').click();
+    });
+
+    // Pictionary bits, yeah you can't break what is already broken. move along.
+    $('#game_pictionary_start').on('click', function() {
+        $('#game_pictionary_start').hide();
+        $('#game_pictionary_correct').show();
+        $('#game_pictionary_skip').show();
+        socket.emit('pictionary start');
+    });
+    $('#game_pictionary_correct').on('click', function() {
+        $('#game_pictionary ul li.active').addClass('correct');
+        socket.emit('pictionary correct');
+    });
+    // Look at all these event handlers, its miraculous that this all even works tbh
+    $('#game_pictionary_end').on('click', function() {
+        $('#game_pictionary_start').hide();
+        $('#game_pictionary_correct').hide();
+        $('#game_pictionary_skip').hide();
+        socket.emit('pictionary end');
+    });
+    $('#game_pictionary_skip').on('click', function() {
+        $('#game_pictionary ul li.active').addClass('skip');
+        socket.emit('pictionary skip');
+    });
+
+    $('#sounds button').on('click', function() {
+        socket.emit('sound play', $(this).data('sound'));
+    });
+
+    $('#titles button').on('click', function() {
+        var title = {title: $(this).data('title'), audio: null};
+        if ($(this).data('audio')) {
+            title.audio = $(this).data('audio');
+        }
+        socket.emit('title show', title);
+    });
+
+
+ // Well there you go, we are out of the valley javascript...
+ //
+ // Ahead of you is a stream...
+ // Adventurer: [n,e,s,w,look,pickup,help]
+ //                                  _
+ //                        .-.      / \        _
+ //            ^^         /   \    /^./\__   _/ \
+ //          _        .--'\/\_ \__/.      \ /    \  ^^  ___
+ //         / \_    _/ ^      \/  __  :'   /\/\  /\  __/   \
+ //        /    \  /    .'   _/  /  \   ^ /    \/  \/ .`'\_/\
+ //       /\/\  /\/ :' __  ^/  ^/    `--./.'  ^  `-.\ _    _:\ _
+ //      /    \/  \  _/  \-' __/.' ^ _   \_   .'\   _/ \ .  __/ \
+ //    /\  .-   `. \/     \ / -.   _/ \ -. `_/   \ /    `._/  ^  \
+ //   /  `-.__ ^   / .-'.--'    . /    `--./ .-'  `-.  `-. `.  -  `.
+ // @/        `.  / /      `-.   /  .-'   / .   .'   \    \  \  .-  \%
+ // @(88%@)@%% @)&@&(88&@.-_=_-=_-=_-=_-=_.8@% &@&&8(8%@%8)(8@%8 8%@)%
+ // @88:::&(&8&&8::JGS:&`.~-_~~-~~_~-~_~-~~=.'@(&%::::%@8&8)::&#@8::::
+ // `::::::8%@@%:::::@%&8:`.=~~-.~~-.~~=..~'8::::::::&@8:::::&8::::::'
+ //  `::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::'
+ //                     "Thankfully no one got hurt."
+ //                             - Some person in the news, probably
+});
